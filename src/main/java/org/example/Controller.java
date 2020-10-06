15a671d6dee882b029b5b3a34166091fa12e80c4
hfh3oa <ddr.dydy.0557@gmail.com>
2020-10-06 22:52:31 +0900

    [add] lambok サンプル

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,13 +1,16 @@
 package org.example;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
     @RequestMapping("/")
     public String index() {
-        return "\nHello World!";
+        DataEntity dataEntity = new DataEntity();
+        dataEntity.setId("0001");
+
+        return "\n" + dataEntity.getId();
     }
 }
---
+++ src/main/java/org/example/DataEntity.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class DataEntity {
+    private String id;
+    private String name;
+    private String remark;
+}
